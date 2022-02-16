c87cc44d1b32142181018f8a9cd79da68c39198c
Jakub <xchrappaj@stuba.sk>
2022-02-16 13:50:28 +0100

    4

---
+++ src/main/java/sk/stuba/fei/uim/oop/Suma_1_az_daco.java
@@ -0,0 +1,13 @@
+package sk.stuba.fei.uim.oop;
+
+public class Suma_1_az_daco {
+
+    public static void main(String[] parametre){
+
+        int n = Integer.parseInt(parametre[2]);
+
+        System.out.print("Výsledok je: ");
+        System.out.println( 1 * (1+1) / 2 );
+    }
+
+}
