4a57ed1271dc40851c00d719e0ccff6897321291
Jakub <xchrappaj@stuba.sk>
2022-02-16 13:54:39 +0100

    cviko1 mini prog

--- src/main/java/sk/stuba/fei/uim/oop/Suma_1_az_daco.java
+++ src/main/java/sk/stuba/fei/uim/oop/Suma_1_az_daco.java
@@ -2,12 +2,12 @@ package sk.stuba.fei.uim.oop;
 
 public class Suma_1_az_daco {
 
-    public static void main(String[] parametre){
+    public static void main(String[] parameter){
 
-        int n = Integer.parseInt(parametre[2]);
+        int n = Integer.parseInt(parameter[0]);
 
         System.out.print("Výsledok je: ");
-        System.out.println( 1 * (1+1) / 2 );
+        System.out.println( n * (n+1) / 2 );
     }
 
 }
